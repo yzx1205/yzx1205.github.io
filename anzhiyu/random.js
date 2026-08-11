803b714af17a2b1f94d8ac49f59cab6a2729e297
yzx <1046877249@qq.com>
2026-08-11 10:08:32 +0800

    Site updated: 2026-08-11 10:08:24

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2026/08/06/hello-world/"];function toRandomPost(){
+var posts=["2026/08/11/Git手册/","2026/08/06/hello-world/","2026/08/07/vscode-cmake-vcpkg项目模板/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
